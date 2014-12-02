2f6def248f98be5446a268633cf272b5d84f54d8
Abhishek <abhiver222@gmail.com>
2014-12-02 17:35:25 -0600

    some more DB changes

--- MyLastNight/src/com/example/mylastnight/drinkmenu.java
+++ MyLastNight/src/com/example/mylastnight/drinkmenu.java
@@ -24,140 +24,146 @@ public class drinkmenu extends dataclass implements OnItemSelectedListener {
 	Spinner commonnames;
 	Spinner specialnames;
 	TextView display;
-	int pos,i=0;
+	int pos, i = 0;
 	String name;
-	String[] names = {" "," "," "," "," "," "," "," "," "," "," "};
+	String[] names = { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
 	boolean first = true;
-	int size=1;
-	
+	int size = 1;
+
 	datasource dtsrc;
-	
+
 	public void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.drinkmenu);
-		
+
 		commonnames = (Spinner) findViewById(R.id.spinner1);
 		specialnames = (Spinner) findViewById(R.id.spinner2);
 		display = (TextView) findViewById(R.id.selected);
-		
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-		
-		ArrayAdapter<CharSequence> cname = ArrayAdapter.createFromResource(this,R.array.commonmenudisplay, android.R.layout.simple_spinner_item);
-		ArrayAdapter<CharSequence> spname = ArrayAdapter.createFromResource(this,R.array.specialmenudisplay, android.R.layout.simple_spinner_item);
-		
-		
-		
-		//ArrayAdapter<String> cname = new ArrayAdapter<String>(drinkmenu.this,R.array.commonmenudisplay,android.R.layout.simple_spinner_item);
-		//ArrayAdapter<String> spname = new ArrayAdapter<String>(drinkmenu.this, R.array.specialmenudisplay,android.R.layout.simple_spinner_item);
 
-		//cname.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		//spname.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-       		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		ArrayAdapter<CharSequence> cname = ArrayAdapter.createFromResource(
+				this, R.array.commonmenudisplay,
+				android.R.layout.simple_spinner_item);
+		ArrayAdapter<CharSequence> spname = ArrayAdapter.createFromResource(
+				this, R.array.specialmenudisplay,
+				android.R.layout.simple_spinner_item);
+
+		// ArrayAdapter<String> cname = new
+		// ArrayAdapter<String>(drinkmenu.this,R.array.commonmenudisplay,android.R.layout.simple_spinner_item);
+		// ArrayAdapter<String> spname = new
+		// ArrayAdapter<String>(drinkmenu.this,
+		// R.array.specialmenudisplay,android.R.layout.simple_spinner_item);
+
+		// cname.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// spname.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
 		commonnames.setAdapter(cname);
 		specialnames.setAdapter(spname);
-		
+
 		commonnames.setOnItemSelectedListener(this);
 		specialnames.setOnItemSelectedListener(this);
 		dtsrc = new datasource(this);
 		dtsrc.open();
-		
-		//List<drinkval> drinks = dtsrc.findall();//put where ever data is needed
-		
-		
-		
+		Log.i("Data", "Datasource created new table");
+
+		// List<drinkval> drinks = dtsrc.findall();//put where ever data is
+		// needed
+
 	}
 
 	@Override
 	public void onItemSelected(AdapterView<?> spin, View view, int position,
 			long a) {
-		
-		
-		switch(spin.getId()){
+
+		switch (spin.getId()) {
 		case R.id.spinner1:
 			drinkmenu.this.name = spin.getItemAtPosition(position).toString();
-			String name1 = drinkmenu.this.name; 
+			String name1 = drinkmenu.this.name;
 			createdata(name);
-		
+
 		case R.id.spinner2:
 			drinkmenu.this.pos = position;
 			drinkmenu.this.name = spin.getItemAtPosition(pos).toString();
-			String name = drinkmenu.this.name; 
-			if(first){
-				names[i]=(drinkmenu.this.name);
+			String name = drinkmenu.this.name;
+			if (first) {
+				names[i] = (drinkmenu.this.name);
 				createdata(names[i]);
-				display.setText(Arrays.toString(names).replaceAll("\\[", "").replaceAll("\\]", "").replaceAll(",", " "));
+				display.setText(Arrays.toString(names).replaceAll("\\[", "")
+						.replaceAll("\\]", "").replaceAll(",", " "));
 				i++;
 				first = false;
+			} else {
+				names[i] = (drinkmenu.this.name + " + ");
+				createdata(names[i]);
+				display.setText(Arrays.toString(names).replaceAll("\\[", "")
+						.replaceAll("\\]", "").replaceAll(",", " "));
+				i++;
 			}
-			else{
-			names[i]=(drinkmenu.this.name+" + ");
-			createdata(names[i]);
-			display.setText(Arrays.toString(names).replaceAll("\\[", "").replaceAll("\\]", "").replaceAll(",", " "));
-			i++;
-			}
-		
-		//createdata(name); remove if wanted
-		}//end of switch
+
+			// createdata(name); remove if wanted
+		}// end of switch
 	}
 
 	@Override
 	public void onNothingSelected(AdapterView<?> arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu){
+	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.topactionbuttons, menu);
-        return true;
+		getMenuInflater().inflate(R.menu.topactionbuttons, menu);
+		return true;
 	}
-	
+
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle presses on the action bar items
-	    size++;
+		// Handle presses on the action bar items
+		size++;
 		switch (item.getItemId()) {
-	        case R.id.action_settings:
-	        	for(int i=0;i<names.length;i++)
-	        		names[i]=" ";
-	        	
-	        	
-	        	
-	            display.setText(Arrays.toString(names).replaceAll("\\[", "").replaceAll("\\]", "").replaceAll(",", " "));
-	            Intent back = new Intent(drinkmenu.this,counting.class);
-	           
-	            startActivity(back);
-	            return true;
-	            
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+		case R.id.action_settings:
+			for (int i = 0; i < names.length; i++)
+				names[i] = " ";
+
+			display.setText(Arrays.toString(names).replaceAll("\\[", "")
+					.replaceAll("\\]", "").replaceAll(",", " "));
+			Intent back = new Intent(drinkmenu.this, counting.class);
+
+			startActivity(back);
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
-	
-	protected void onResume(){
+
+	protected void onResume() {
 		super.onResume();
 		dtsrc.open();
 	}
-	
-	protected void onPause(){
+
+	protected void onPause() {
 		super.onPause();
 		dtsrc.close();
 	}
-	
-	private void createdata(String all){
-		if(all.equals(""))
+
+	private void createdata(String all) {
+		if (all.equals(""))
 			return;
-		else{
-		int a = all.indexOf(',');
-		String name = all.substring(0,a);
-		double cont  = Double.parseDouble(all.substring(a+1));
-		drinkval d1 = new drinkval();
-		d1.setname(name);
-		d1.setalval(cont);
-		Log.i(sqldb1.LOGTAG, "Drink created with id "+ d1.getid());
-		dtsrc.create(d1);
-		
+		else {
+			int a = all.indexOf(',');
+			if (a == -1)
+				return;
+			String name = all.substring(0, a);
+			double cont = Double.parseDouble(all.substring(a + 1,a+3));
+			drinkval d1 = new drinkval();
+			d1.setname(name);
+			d1.setalval(cont);
+			Log.i(sqldb1.LOGTAG, "Drink created with id " + d1.getid());
+			dtsrc.create(d1);
+
 		}
 	}
 
